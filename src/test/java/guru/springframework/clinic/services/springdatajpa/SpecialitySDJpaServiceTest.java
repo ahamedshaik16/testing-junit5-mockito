355a253ce29ef4715489fa7b42a391a35f38fabe
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-19 08:39:17 +0530

    crud operations test using mockito

--- src/test/java/guru/springframework/clinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/clinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -24,6 +24,14 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
+    void testDeleteByObject() {
+        Speciality speciality = new Speciality();
+        service.delete(speciality);
+
+        verify(specialtyRepository).delete(any(Speciality.class));
+    }
+
+    @Test
     void findByIdTest() {
         Speciality speciality = new Speciality();
 
@@ -32,7 +40,7 @@ class SpecialitySDJpaServiceTest {
         Speciality foundSpeciality = service.findById(1L);
 
         assertThat(foundSpeciality).isNotNull();
-        verify(specialtyRepository).findById(1L);
+        verify(specialtyRepository).findById(anyLong());
     }
 
     @Test
